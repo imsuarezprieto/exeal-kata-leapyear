23596d99c8968f184cd0108ebd4338adc2702def
issuarez <issuarez@aidacanarias.com>
2021-10-25 20:47:45 +0200

    Added test for second condition.
    Years divisible by 100 and not by 400 should not be leap years. So is not year 100.
    Minimal testing code formatting.

--- LeapYear.Test/LeapYearTest.cs
+++ LeapYear.Test/LeapYearTest.cs
@@ -7,12 +7,17 @@ namespace LeapYear.Test
 	public class LeapYearTest
 	{
 		[Theory]
-		[InlineData(400)]
-		[InlineData(800)]
-		[InlineData(1200)]
+		[InlineData( 400 )]
+		[InlineData( 800 )]
+		[InlineData( 1200 )]
 		public void YearsDivisibleBy400_ShouldBe_LeapYears (
 				Int32 year) {
 			Assert.True( LeapYear.IsLeapYear( year ) );
+		}
+
+		[Fact]
+		public void Year100_ShouldNotBe_LeapYear () {
+			Assert.False( LeapYear.IsLeapYear( 100 ) );
 		}
 	}
 }
