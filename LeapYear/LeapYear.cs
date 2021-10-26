a06741d5cd324f95d74b6430a9f618116fc33c94
issuarez <issuarez@aidacanarias.com>
2021-10-26 14:52:17 +0200

    Plain implementatio for fourth condition.
    The year 1 is no a leap one.

--- LeapYear/LeapYear.cs
+++ LeapYear/LeapYear.cs
@@ -7,6 +7,7 @@ namespace LeapYear
 		public static Boolean IsLeapYear (
 				Int32 year) {
 			if ((year % 100 == 0) && (year % 400 != 0)) return false;
+			if (year % 4 != 0) return false;
 			return true;
 		}
 	}
